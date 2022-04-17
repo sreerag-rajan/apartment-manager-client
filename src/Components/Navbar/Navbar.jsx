4b86430c6926dcffc36b07c81e373b0f4c840377
sreerag-rajan <85482640+sreerag-rajan@users.noreply.github.com>
2022-04-17 19:25:29 +0530

    some styling changes in navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,15 +6,17 @@ import { useSelector } from "react-redux";
 export const Navbar = ()=>{
     const user = useSelector((store)=>store.auth.user);
     return (
-        <Box display={"flex"} justifyContent="space-around" border="1px solid black" padding="20px" backgroundColor="teal" color="white">
+        <Box display={"flex"} justifyContent="space-between" border="1px solid black" padding="20px" backgroundColor="teal" color="white">
             <Link to="/">Home</Link>
-            {user? <div>
-                <Link to="/addflat">Add Flat</Link>
-                <Link to="/addresident">Add Resident</Link>
-                </div>:<div>
-                    <Link to="/login">Login</Link>
-                <Link to="/register">Register</Link>
-            </div>}
+            {user? <Box display={"flex"} justifyContent="space-around" gap="50px">
+                <div><Link to="/addflat">Add Flat</Link></div>
+                <div><Link to="/addresident">Add Resident</Link></div>
+                
+                </Box>:<Box display={"flex"} justifyContent="space-between" gap="50px">
+                    <div><Link to="/login">Login</Link></div>
+                    <div><Link to="/register">Register</Link></div>
+                    
+            </Box>}
             
             
         </Box>
